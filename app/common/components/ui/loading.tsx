09c9a4ee172deae21a06e93ad556b5d9da2ed776
bysxx <0415white@gmail.com>
2024-01-22 18:07:16 +0900

    feat: update typescript version

--- app/common/components/ui/loading.tsx
+++ app/common/components/ui/loading.tsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+        className="mr-2 size-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
